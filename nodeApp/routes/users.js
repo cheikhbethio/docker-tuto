b999d626de275967e32c56a50d419f0f62969717
cheikhbethio <sow.moussa.dev@gmail.com>
2020-05-13 04:52:36 +0200

    angular with tests + nodejs +  mongo

--- nodeApp/routes/users.js
+++ nodeApp/routes/users.js
@@ -1,9 +1,48 @@
 var express = require('express');
 var router = express.Router();
+const mongoose = require('mongoose');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+// MongoDB URL from the docker-compose file
+const dbHost = 'mongodb://moussa:keunzeune@db/tuto-docker-db';
+
+// Connect to mongodb
+mongoose.connect(dbHost);
+
+// create mongoose schema
+const userSchema = new mongoose.Schema({
+  name: String,
+  age: Number
 });
+
+// create mongoose model
+const User = mongoose.model('User', userSchema);
+
+
+router.get('', (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) {
+      return res.status(500).send(error);
+    }
+    return res.status(200).json(users);
+  });
+});
+
+
+router.post('', (req, res) => {
+  let user = new User({
+    name: req.body.name,
+    age: req.body.age
+  });
+
+  user.save(error => {
+    if (error)
+      return res.status(500).send(error);
+
+    return res.status(201).send({
+      message: 'User created successfully'
+    });
+  });
+});
+
 
 module.exports = router;
